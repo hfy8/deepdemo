cee8c79f3a4e35bb6fc8fb677ccebd20faf1dee8
Jeffrey <1003393709@qq.com>
2018-12-29 15:32:40 +0800

    java深度计算

---
+++ src/main/java/com/jeffrey/verifynum/VerifynumApplication.java
@@ -0,0 +1,14 @@
+package com.jeffrey.verifynum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifynumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerifynumApplication.class, args);
+    }
+
+}
+
